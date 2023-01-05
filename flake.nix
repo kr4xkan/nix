75db8c45073bbe3fe1d06cdff5d707b4ddd5232a
Hugo Baptista <kr4xkan@Hydra.lan>
2023-01-05 18:44:36 +0100

    darwin conf

--- flake.nix
+++ flake.nix
@@ -1,22 +1,18 @@
 {
   description = "Hydra";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.11-darwin";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, darwin }: {
+  outputs = { self, nixpkgs, darwin }: {
     darwinConfigurations."Hydra" = darwin.lib.darwinSystem {
-
       system = "aarch64-darwin";
       modules = [
-        home-manager.darwinModules.home-manager
-        ./hosts/Hydra/default.nix
+        ./hosts/Hydra/darwin-configuration.nix
       ];
     };
   };
